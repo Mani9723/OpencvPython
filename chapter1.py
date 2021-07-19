1c89b3cec29c5a76424bda22e3b8ec3aa6ab135d
Mani9732 <mani.shah23@gmail.com>
2021-07-18 21:56:12 -0400

    Added webcam facetracking

--- chapter1.py
+++ chapter1.py
@@ -9,6 +9,7 @@ import cv2
 # Part 2: Read and open video
 
 # cap = cv2.VideoCapture("Resources/test_video.mp4")
+faceCascade = cv2.CascadeClassifier("Resources/haarcascade_frontalface_default.xml")
 cap = cv2.VideoCapture(0)
 cap.set(3,640) # Set height
 cap.set(4,480) # set width
@@ -16,6 +17,11 @@ cap.set(4,480) # set width
 
 while True:
     success, img = cap.read() # default webcam, parameter >= 0 external webcam
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(imgGray, 1.1, 2)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
     cv2.imshow("Video",img)
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
